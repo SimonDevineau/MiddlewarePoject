758e6bc3b7151e05dedde8b3ef5b40282b5d4231
PierreReliquet <pierre.reliquet@gmail.com>
2012-12-13 13:23:34 +0100

    Adding the role

---
+++ examination-middleware/src/main/java/fr/emn/examination/model/Role.java
@@ -0,0 +1,13 @@
+/**
+ * Part of the project : examination-middleware
+ * fr.emn.examination.model.Roles.java Created by : pierre
+ */
+package fr.emn.examination.model;
+
+/**
+ * @author Pierre Reliquet
+ * 
+ */
+public enum Role {
+    ADMINISTRATOR, STUDENT
+}
