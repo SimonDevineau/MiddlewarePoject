b126c3437458077ee7ff763699270c49ab9951fd
Dayde <cedric.nis@gmail.com>
2012-12-19 19:14:07 +0100

    Implémentation de la gestion des rôles

--- examination-middleware/src/main/java/fr/emn/examination/view/JavaCode.java
+++ examination-middleware/src/main/java/fr/emn/examination/view/JavaCode.java
@@ -1,31 +1,30 @@
+package fr.emn.examination.view;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
 public class JavaCode {
 
-	private HashMap<String, CodeQuestion> questions;
+    private HashMap<String, CodeQuestion> questions;
 
-	private ArrayList<String> 
-	roughCode,
-	questionIds;
-	
-	public JavaCode() {
-		questions = new HashMap<>();
-		roughCode = new ArrayList<>();
-		questionIds = new ArrayList<>();
-	}
+    private ArrayList<String>             roughCode, questionIds;
 
-	public HashMap<String, CodeQuestion> getQuestions() {
-		return questions;
-	}
+    public JavaCode() {
+	questions = new HashMap<String, CodeQuestion>();
+	roughCode = new ArrayList<String>();
+	questionIds = new ArrayList<String>();
+    }
 
-	public ArrayList<String> getRoughCode() {
-		return roughCode;
-	}
+    public HashMap<String, CodeQuestion> getQuestions() {
+	return questions;
+    }
 
-	public ArrayList<String> getQuestionIds() {
-		return questionIds;
-	}
+    public ArrayList<String> getRoughCode() {
+	return roughCode;
+    }
+
+    public ArrayList<String> getQuestionIds() {
+	return questionIds;
+    }
 
 }
