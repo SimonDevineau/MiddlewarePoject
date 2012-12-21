04d26a3b15941f1b728a64d4d27fd40a0e1ba5f6
Frederic <frederic.lucas@unicorntraining.com>
2012-12-21 14:17:30 +0100

    déplacé la partie du parsing du code java et modifications du parser

--- examination-middleware/src/main/java/fr/emn/examination/model/JavaCode.java
+++ examination-middleware/src/main/java/fr/emn/examination/model/JavaCode.java
@@ -1,4 +1,4 @@
-package fr.emn.examination.view;
+package fr.emn.examination.model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- examination-middleware/src/main/java/fr/emn/examination/parser/JavaCodeParser.java
+++ examination-middleware/src/main/java/fr/emn/examination/parser/JavaCodeParser.java
@@ -1,5 +1,4 @@
-package fr.emn.examination.view;
-
+package fr.emn.examination.parser;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -16,8 +15,9 @@ public class JavaCodeParser {
 	QUESTION_START_REGEX = "<\\ *question\\ *id\\ *=\\ *\"(?<questionId>[a-zA-Z0-9]*)\"\\ *>",
 	QUESTION_END_REGEX = "<\\ */\\ *question\\ *>",
 	SEGMENT_START_REGEX = "<\\ *segment\\ *id\\ *=\\ *\"(?<segmentId>[a-zA-Z0-9]*)\"\\ *>",
-	SEGMENT_END_REGEX = "<\\ */\\ *segment\\ *>";
-	
+	SEGMENT_END_REGEX = "<\\ */\\ *segment\\ *>",
+	VALIDATION_REGEX = "<\\ *validation\\ */\\ *>";
+
 	private JavaCode javaCode;
 
 	public JavaCodeParser(String code) {
@@ -27,35 +27,36 @@ public class JavaCodeParser {
 
 	public void parse(String code) {
 		int currentIndex = 0;
-		
+
 		boolean 
 		questionStarted = false,
 		segmentStarted = false;
-		
+
 		HashMap<String, String> segments = null;
-		
+
 		ArrayList<String> 
 		displayedCode = null,
 		segmentIds = null;
-		
+
 		String 
 		questionId = null, 
 		segmentId = null;
-		
+
 		while(code.indexOf("/*", currentIndex)!=-1) {
-			
+
 			int 
 			commentStartIndex = code.indexOf("/*", currentIndex),
 			commentEndIndex = code.indexOf("*/", commentStartIndex);
 			String 
 			previousCode = code.substring(currentIndex, commentStartIndex),
 			comment = code.substring(commentStartIndex+2, commentEndIndex);
-			
+
 			Matcher questionStart = Pattern.compile(QUESTION_START_REGEX).matcher(comment);
 			Matcher segmentStart = Pattern.compile(SEGMENT_START_REGEX).matcher(comment);
 			Matcher segmentEnd = Pattern.compile(SEGMENT_END_REGEX).matcher(comment);
 			Matcher questionEnd = Pattern.compile(QUESTION_END_REGEX).matcher(comment);
-			
+			Matcher validation = Pattern.compile(VALIDATION_REGEX).matcher(comment);
+
 			if(questionStart.find() && !questionStarted && !segmentStarted) {
 				javaCode.getRoughCode().add(previousCode);
 				questionStarted = true;
@@ -71,39 +72,48 @@ public class JavaCodeParser {
 				segmentId = segmentStart.group("segmentId");
 				segmentIds.add(segmentId);
 			}
-			
+
 			else if(segmentEnd.find() && questionStarted && segmentStarted) {
 				segmentStarted = false;
 				segments.put(segmentId, previousCode);
 			}
-			
+
 			else if(questionEnd.find() && questionStarted && !segmentStarted) {
 				questionStarted = false;
 				javaCode.getQuestionIds().add(questionId);
 				displayedCode.add(previousCode);
 				CodeQuestion newQuestion = new CodeQuestion(javaCode, questionId, displayedCode, segmentIds, segments);
 				javaCode.getQuestions().put(questionId, newQuestion);
-				
+
 			}
-			
+
+			else if(validation.find()) {
+				javaCode.getRoughCode().add(previousCode);
+				javaCode.getQuestionIds().add("#validation");
+			}
+
 			currentIndex = commentEndIndex+2;
 		}
-		
+		javaCode.getRoughCode().add(code.substring(currentIndex, code.length()));
 	}
 	
+	public JavaCode getJavaCode() {
+		return javaCode;
+	}
+
 	public static void main(String[] args) {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(new File("Test.cor.java")));
 			String s = "";
 			while(br.ready())
 				s+=(char)br.read();
-			new JavaCodeParser(s);
+			System.out.println(new JavaCodeParser(s).getJavaCode());
 			br.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
