b5525c1050d1440aea5600c658fb1a84cb7b6790
osiris <osirisxxxl@gmail.com>
2012-12-21 16:16:27 +0100

    File upload foncitonnel pour un sujet xml par l'administrateur

--- examination-middleware/src/main/java/fr/emn/examination/controller/FileUploadController.java
+++ examination-middleware/src/main/java/fr/emn/examination/controller/FileUploadController.java
@@ -1,14 +1,22 @@
 package fr.emn.examination.controller;
 
+
 import javax.faces.application.FacesMessage;  
 import javax.faces.context.FacesContext;  
   
 import org.primefaces.event.FileUploadEvent;  
-import org.primefaces.model.UploadedFile;  
+
+import fr.emn.examination.persistence.Factory;
+import fr.emn.examination.persistence.GenericDAO;
   
 public class FileUploadController {  
   
     public void handleFileUpload(FileUploadEvent event) {  
+    	GenericDAO<String> dao = Factory.getExamenXMLDAO();
+    	String file=new String(event.getFile().getContents());
+	    
+    	dao.create(file);
+    	
         FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");  
         FacesContext.getCurrentInstance().addMessage(null, msg);  
     }  
