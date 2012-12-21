26a86ee82ac0e45c2d500ae1f94f7fbe2260db17
osiris <osirisxxxl@gmail.com>
2012-12-21 15:04:59 +0100

    Mise en place de l'upload de sujet d'examen v1

---
+++ examination-middleware/src/main/java/fr/emn/examination/controller/FileUploadController.java
@@ -0,0 +1,15 @@
+package fr.emn.examination.controller;
+
+import javax.faces.application.FacesMessage;  
+import javax.faces.context.FacesContext;  
+  
+import org.primefaces.event.FileUploadEvent;  
+import org.primefaces.model.UploadedFile;  
+  
+public class FileUploadController {  
+  
+    public void handleFileUpload(FileUploadEvent event) {  
+        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }  
+}  
